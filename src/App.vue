a457fc6013bc6217180131ee57b9ce0dbd050697
zxy874175242 <874175242@qq.com>
2020-06-13 04:20:38 +0800

    前段进度

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app">
+<!--      <video autoplay loop id="bgvid">-->
+<!--        <source src="http://works.juanmaotu.top/static/2.mp4" type="video/mp4">-->
+<!--      </video>-->
+
     <div class="bg">
     </div>
     <router-view/>
   </div>
 </template>
+
+<!--http://works.juanmaotu.top/static/1.mp4-->
 
 <script>
 export default {
@@ -16,19 +22,30 @@ export default {
 :root {
   --blur: 5px;
 }
+video#bgvid {
+  position: fixed; right: 0; bottom: 0;
+  min-width: 100%; min-height: 100%;
+  width: auto;
+  height: auto;
+  z-index: -100;
+  /*background: url("../static/img/bg.jpg") no-repeat;*/
+  background: rgba(255,255,255,1);
+  background-size: cover;
+  /*filter: blur(5px)*/
+}
 .bg{
   position: absolute;
   left: 0;
   top: 0;
   width: 100%;
   height: 100%;
-  background: url("../static/img/bg.jpg") no-repeat;
+  background: #3aafa9;
   background-size: cover;
-  filter: blur(var(--blur));
-  -webkit-filter:blur(var(--blur));
-  -moz-filter:blur(var(--blur));
-  -ms-filter:blur(var(--blur));
-  -o-filter:blur(var(--blur));
+  /*filter: blur(var(--blur));*/
+  /*-webkit-filter:blur(var(--blur));*/
+  /*-moz-filter:blur(var(--blur));*/
+  /*-ms-filter:blur(var(--blur));*/
+  /*-o-filter:blur(var(--blur));*/
   z-index: -1;
 }
 </style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,44 +1,49 @@
 <template>
   <div id="loginWrapper">
+    <div class="title-is" @click="isLoginPage = true;toLogin()">登录</div>
+    <div class="title-is2" @click="isLoginPage = false;toRegister()">注册</div>
     <div>
       <div id="nickWrapper" v-show="isLoginPage">
-        <h3>Login</h3>
+<!--        <h3>Login</h3>-->
         <div class="formItem">
           <label for="LUsername">用户名：</label>
-          <input v-model="LUsername" @keyup.enter="signIn" type="text" name="LUsername" placeholder="username">
+          <input v-model="LUsername" @keyup.enter="signIn" type="text" name="LUsername" placeholder="账号 / 用户名" onfocus="this.placeholder=''">
         </div>
         <div class="formItem">
           <label for="LPassword">密码：</label>
-          <input v-model="LPassword" @keyup.enter="signIn" type="password" name="LPassword" placeholder="password">
+          <input v-model="LPassword" @keyup.enter="signIn" type="password" name="LPassword" placeholder="密码" onfocus="this.placeholder=''">
         </div>
         <div class="formItem">
-          <a class="toBtn" href="javascript:;" @click="isLoginPage = !isLoginPage">还没有账号？注册一个吧</a>
-          <p id="loginBtn" @click="signIn">登录</p>
+          <a class="toBtn" href="javascript:;" @click="isLoginPage = !isLoginPage;toRegister()">还没有账号？注册一个吧</a>
+          <div class="btn" id="loginBtn" @click="signIn">登录</div>
         </div>
       </div>
       <div id="registWrapper" v-show="!isLoginPage">
-        <h3>Register</h3>
+<!--        <h3>Register</h3>-->
         <div class="formItem">
           <label for="RUsername">用户名：</label>
-          <input v-model="RUsername" @keyup.enter="signUp" type="text" name="RUsername" placeholder="username">
+          <input v-model="RUsername" @keyup.enter="signUp" type="text" name="RUsername" placeholder="用户名" onfocus="this.placeholder=''">
         </div>
         <div class="formItem">
           <label for="RPassword">密码：</label>
-          <input v-model="RPassword" @keyup.enter="signUp" type="password" name="RPassword" placeholder="password">
+          <input v-model="RPassword" @keyup.enter="signUp" type="password" name="RPassword" placeholder="密码" onfocus="this.placeholder=''">
         </div>
         <div class="formItem">
-          <a class="toBtn" href="javascript:;" @click="isLoginPage = !isLoginPage">已有账号，快去登录吧</a>
-          <p @click="signUp">注册</p>
+          <a class="toBtn" href="javascript:;" @click="isLoginPage = !isLoginPage;toLogin()">已有账号，快去登录吧</a>
+          <div class="btn" @click="signUp">注册</div>
         </div>
       </div>
     </div>
     <div class="repeatDialog" :style="{display: repeatdialog ? 'block' : 'none'}">
       <div>该账号在另一地点登录，确定继续登录吗？</div>
-      <p @click="forceOffLine">确定</p><p @click="repeatdialog = !repeatdialog">取消</p>
+      <div style="width: 100%;height: 50px"></div>
+      <div class="btn" @click="forceOffLine" style="right: 130px;margin-top: -30px">确定</div>
+      <div class="btn" @click="repeatdialog = !repeatdialog" style="margin-top: -30px">取消</div>
     </div>
   </div>
 </template>
 <script>
+import Push from 'push.js'
 export default {
   data () {
     return {
@@ -49,6 +54,9 @@ export default {
       RPassword: '',
       repeatdialog: false
     }
+  },
+  created () {
+    Push.Permission.request()
   },
   methods: {
     signUp () {
@@ -113,6 +121,29 @@ export default {
       }).catch(err => {
         console.log(err)
       })
+    },
+    toRegister () {
+      var is = document.getElementsByClassName('title-is')[0]
+      var is2 = document.getElementsByClassName('title-is2')[0]
+      is.style.transform = 'scale(0.8,0.8) translate(0px,6px)'
+      is.style.color = '#def2f1'
+      is.style.backgroundColor = '#2b7a78'
+      is2.style.transform = 'scale(1,1) translate(0px,0px)'
+      is2.style.color = '#2b7a78'
+      is2.style.backgroundColor = '#def2f1'
+      // is.style.opacity = 0.5
+    },
+    toLogin () {
+      // alert('??')
+      var is = document.getElementsByClassName('title-is')[0]
+      var is2 = document.getElementsByClassName('title-is2')[0]
+      is.style.transform = 'scale(1,1) translate(0px,0px)'
+      is2.style.transform = 'scale(0.8,0.8) translate(0px,6px)'
+      is2.style.color = '#def2f1'
+      is2.style.backgroundColor = '#2b7a78'
+      is.style.color = '#2b7a78'
+      is.style.backgroundColor = '#def2f1'
+      // is.style.opacity = 0.5
     }
   },
   socket: {
@@ -120,20 +151,33 @@ export default {
 }
 </script>
 <style scoped>
+  input::placeholder {
+    padding-left: 3px;
+    font-size: 12px;
+    font-family: Arial;
+    letter-spacing: 3px;
+    color: darkgrey;
+    text-align: left;
+  }
 #loginWrapper{
+  width: 300px;
+  height: 200px;
   padding-left: 20px;
   padding-right: 20px;
   position: absolute;
   left: 50%;
   top: 50%;
   transform: translate(-50%,-50%);
-  border: 1px solid rgb(233, 230, 230);
-  background: rgb(255, 255, 255);
-  border-radius: 10px;
+  border: 1px solid rgb(244, 251, 255);
+  background: rgba(255, 255, 255, 1);
+  border-radius: 0px 0px 10px 10px;
+  box-shadow: 0px 0px 25px rgba(0, 0, 0, 0.1);
+  font-family: "Gen Jyuu Gothic Regular";
+  color: #3aafa9;
 }
 #nickWrapper,#registWrapper{
-  margin-top: 30px;
-  margin-bottom: 30px;
+  margin-top: 40px;
+  margin-bottom: 20px;
 }
 h3{
   text-align: center;
@@ -162,7 +206,7 @@ input{
   color: #71b0c9;
 }
 #nickWrapper p,#registWrapper p{
-  padding: 5px 10px;
+  /*padding: 5px 10px;*/
   background: #71b0c9;
   color: white;
   border: 5px;
@@ -171,23 +215,91 @@ input{
 }
 .repeatDialog{
   width: 200px;
+  height: 100px;
   position: absolute;
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
   background: white;
   text-align: center;
-  padding: 10px;
-  border: 1px solid #71b0c9;
+  padding: 20px 15px 15px;
+  border: 1px solid #2b7a78;
   border-radius: 5px;
 }
 .repeatDialog p{
-  display: inline-block;
-  padding: 5px 10px;
-  border-radius: 3px;
-  background: #71b0c9;
-  color:white;
-  margin-right: 5px;
-  cursor: pointer;
+  /*display: inline-block;*/
+  /*padding: 5px 10px;*/
+  /*border-radius: 3px;*/
+  /*background: #71b0c9;*/
+  /*color:white;*/
+  /*margin-right: 5px;*/
+  /*cursor: pointer;*/
 }
+  .title-is{
+    height: 35px;
+    line-height: 35px;
+    width: 145px;
+    position: absolute;
+    margin-top: -46px;
+    margin-left: -21px;
+    /*font-family: 汉仪新人文宋W;*/
+    color: #2b7a78;
+    font-size: 23px;
+    background-color: #def2f1;
+    z-index: -50;
+    padding: 5px 20px;
+    border-radius: 10px 0px 0px 0px;
+    /*letter-spacing: -2px;*/
+    transition: transform 0.4s ease,color 0.2s ease,background-color 0.2s ease;
+    cursor: pointer;
+  }
+  /*.title-is:hover{*/
+  /*  transform: scale(0.8,0.8) translate(0px,6px);*/
+  /*}*/
+  .title-is2{
+  text-align: right;
+    height: 35px;
+    line-height: 35px;
+    width: 135px;
+    position: absolute;
+    margin-top: -46px;
+    margin-left: 146px;
+    /*font-family: 汉仪新人文宋W;*/
+    color: #def2f1;
+    font-size: 23px;
+    background-color: #2b7a78;
+    z-index: -200;
+    padding: 5px 20px;
+    border-radius: 0px 10px 0px 0px;
+  transform: scale(0.8,0.8) translate(0px,6px);
+    transition: transform 0.4s ease,color 0.2s ease,background-color 0.2s ease;
+    cursor: pointer;
+    /*letter-spacing: -2px;*/
+
+  }
+  .btn{
+    position: absolute;
+    right: 40px;
+    width: 60px;
+    height: 25px;
+    line-height: 25px;
+    border-radius: 3px;
+    align-items: center;
+    text-align: center;
+    margin-top: -20px;
+    background-color: #2b7a78;
+    color: white;
+    transition: background-color 0.2s ease;
+    cursor: pointer;
+  }
+  .btn:hover{
+    background-color: #204846;
+  }
+  .btn:active{
+    background-color: #3aafa9;
+  }
+  /*.title-is2:hover{*/
+  /*  transform: scale(1,1) translate(0px,0px);*/
+
+  /*}*/
 </style>
